c57139a6e711be641675c27423a25422206abe84
Chris Habgood <chabgood@gmail.com>
2019-08-24 19:53:48 -0500

    added ppd from client to sms

--- app/fah_stats_sms.rb
+++ app/fah_stats_sms.rb
@@ -1,4 +1,5 @@
 require 'bundler/inline'
+require 'net/telnet'
 
 gemfile do
   source 'https://rubygems.org'
@@ -26,11 +27,12 @@ class FahStatsSms
 
   base_uri 'https://api.foldingathome.org/'
 
-  attr_accessor :number, :query, :account_sid, :auth_token, :client, :to, :from
+  attr_accessor :number, :query, :account_sid, :auth_token, :client, :to, :from, :ppd
 
   def initialize()
     @number = 0
     @query = { query: {  passkey: ENV["PASSKEY"], team: ENV["TEAM"], header: { 'Content-Type' => 'application/json' } } }
+    @ppd = 0
     initialize_twilio_info
   end
 
@@ -47,6 +49,7 @@ class FahStatsSms
     file_hash = load_file
     return if api_total[:stats] == file_hash["stats"]
     update_total(api_total)
+    get_ppd
     send_sms(api_total)
   end
 
@@ -55,9 +58,10 @@ class FahStatsSms
   def send_sms(api_total)
     stats = number_to_human(api_total[:stats], precision: 5)
     rank = number_to_human(api_total[:rank], precision: 5)
-    score = number_to_human(api_total[:score], precision:5)
+    score = number_to_human(api_total[:score], precision: 5)
+    ppd = number_to_human(self.ppd, precision: 5)
     self.client.messages.create(from: self.from, to: self.to,
-      body: "Score: #{score} \n Current Team Total: #{stats} \n Total Rank: #{rank}"
+      body: "Score: #{score} \n Current Team Total: #{stats} \n Total Rank: #{rank}, PPD: #{ppd}"
     )
   end
 
@@ -76,6 +80,11 @@ class FahStatsSms
     data_rank = self.class.get('/user/MrMoo').parsed_response
     score = data_rank['teams'][0]['score']
     return { stats: score, rank: data_rank['rank'].to_i, score: data_rank['score'] }
+  end
+
+  def get_ppd
+    pop = Net::Telnet::new("Host" => "localhost", "Port" => 36330)
+    pop.cmd("ppd") { |c| self.ppd = c.scan(/^[0-9]*\.[0-9]*$/).last }
   end
 end
 
